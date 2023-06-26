da2cb1cd0d36d10b050c1d58d44800bf1dc4b36d
Mykhailo Pidhorodetskyi <13919012+Michelio@users.noreply.github.com>
2023-06-26 21:01:27 +0300

    + create user implemented

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,8 +1,63 @@
 import { USER } from "../models/user.js";
 
+const EMAIL_REGEX = /^[a-z0-9](\.?[a-z0-9]){5,30}@gmail\.com$/gi;
+const PHONE_REGEX = /^(\+380)\d{9}/g;
+
+const createErrorMessage = (message) => {
+  return {
+    error: true,
+    message,
+  };
+};
+
+const validateUser = (userData) => {
+  Object.keys(userData).forEach((key) => {
+    if (!USER.hasOwnProperty(key)) {
+      return createErrorMessage(`Invalid property \'${key}\'.`);
+    }
+  });
+
+  if (userData.hasOwnProperty("id")) {
+    return createErrorMessage("You can't use id as a property.");
+  }
+
+  if (userData.firstName?.length <= 0) {
+    return createErrorMessage("Invalid first name.");
+  }
+
+  if (userData.lastNameName?.length <= 0) {
+    return createErrorMessage("Invalid last name.");
+  }
+
+  if (!userData.email?.match(EMAIL_REGEX)) {
+    return createErrorMessage("Invalid email address.");
+  }
+
+  if (!userData.phoneNumber?.match(PHONE_REGEX)) {
+    return createErrorMessage("Invalid phone number.");
+  }
+
+  if (userData.password?.length < 3) {
+    return createErrorMessage("Invalid password.");
+  }
+};
+
 const createUserValid = (req, res, next) => {
-  // TODO: Implement validatior for USER entity during creation
-  next();
+  const userData = req.body;
+
+  if (userData) {
+    const valid = validateUser(userData);
+    if (!!valid) {
+      res.status(400).json(valid);
+    } else {
+      res.status(200);
+      next();
+    }
+  } else {
+    res
+      .status(400)
+      .json(createErrorMessage("User entity to create isn't valid."));
+  }
 };
 
 const updateUserValid = (req, res, next) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,14 +9,37 @@ import { responseMiddleware } from "../middlewares/response.middleware.js";
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.post(
+  "/",
+  createUserValid,
+  (req, res, next) => {
+    try {
+      const newUser = userService.create(req.body);
+      if (newUser) {
+        res.status(200).data = newUser;
+      } else {
+        res.status(400).data = {
+          error: true,
+          message: "Unable to create user. Try again.",
+        };
+      }
+    } catch (err) {
+      res.status(400).data = { error: true, message: err.message };
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
 router.get(
   "/",
   (req, res, next) => {
     try {
-      const data = userService.getAll(req);
-      res.data = data;
+      const data = userService.getAllUsers(req);
+      res.status(200).data = data;
     } catch (err) {
-      res.err = err;
+      res.status(404).data = { error: true, message: err.message };
     } finally {
       next();
     }
