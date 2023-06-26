eb079c79481661e3c6e90387e95aa285dc21614b
Mykhailo Pidhorodetskyi <13919012+Michelio@users.noreply.github.com>
2023-06-27 00:29:51 +0300

    + fighter api implemented

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -5,9 +5,95 @@ import {
   createFighterValid,
   updateFighterValid,
 } from "../middlewares/fighter.validation.middleware.js";
+import { createErrorMessage } from "../utilities/errorMessage.js";
 
 const router = Router();
 
-// TODO: Implement route controllers for fighter
+router.post(
+  "/",
+  createFighterValid,
+  (req, res, next) => {
+    try {
+      const data = req.body;
+      data.name = data.name.toLowerCase();
+      const newFighter = fighterService.create(data);
+      if (newFighter) {
+        res.status(200).data = newFighter;
+      } else {
+        res.status(400).data = createErrorMessage(
+          "Unable to create Fighter. Try again."
+        );
+      }
+    } catch (err) {
+      res.status(400).data = createErrorMessage(err.message);
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.get(
+  "/",
+  (req, res, next) => {
+    try {
+      const data = fighterService.getAllFighters();
+      res.status(200).data = data;
+    } catch (err) {
+      res.status(404).data = createErrorMessage(err.message);
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const data = fighterService.getFighter(req.body.id);
+      res.status(200).data = data;
+    } catch (err) {
+      res.status(404).data = createErrorMessage(err.message);
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.put(
+  "/:id",
+  updateFighterValid,
+  (req, res, next) => {
+    try {
+      const data = req.body;
+      if (data.name) data.name = data.name.toLowerCase();
+      const fighterData = fighterService.update(data);
+      res.status(200).data = fighterData;
+    } catch (err) {
+      res.status(404).data = createErrorMessage(err.message);
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const data = fighterService.delete(req.body.id);
+      res.status(200).data = data;
+    } catch (err) {
+      res.status(404).data = createErrorMessage(err.message);
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
 
 export { router };
--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,62 @@
 import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FighterService {
-  // TODO: Implement methods to work with fighters
+  search(search) {
+    const item = fighterRepository.getOne(search);
+
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+
+  create(fighterData) {
+    const { name } = fighterData;
+    const fighter = this.search({ name });
+
+    if (!fighter) {
+      const newFighter = fighterRepository.create(fighterData);
+      return newFighter;
+    } else {
+      throw new Error("Fighter with that name already exists.");
+    }
+  }
+
+  getAllFighters() {
+    return fighterRepository.getAll();
+  }
+
+  getFighter(fighterId) {
+    const fighter = this.search({ id: fighterId });
+
+    if (fighter) {
+      return fighter;
+    } else {
+      throw new Error("Fighter doesn't exists.");
+    }
+  }
+
+  update(newFighterData) {
+    const { id } = newFighterData;
+    const fighter = this.search({ id });
+
+    if (fighter) {
+      const updatedFighter = fighterRepository.update(id, newFighterData);
+      return updatedFighter;
+    } else {
+      throw new Error("Fighter doesn't exists.");
+    }
+  }
+
+  delete(fighterId) {
+    const fighter = this.search({ id: fighterId });
+
+    if (fighter) {
+      return fighterRepository.delete(fighterId);
+    } else {
+      throw new Error("Fighter with that id doesn't exists.");
+    }
+  }
 }
 
 const fighterService = new FighterService();
